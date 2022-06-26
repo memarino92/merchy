6475bc2676180da05a9ff8f11e05c951272c7ecf
memarino92 <memarino92@gmail.com>
2022-06-26 15:14:13 -0400

    put it all together

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
-const { postSlackMessage } = require('./slack');
 const BandcampClient = require('./bandcamp');
 
 let client = new BandcampClient();
 
-const main = async (client) => {
-  console.log(await client.getOrders(process.env.BANDCAMP_BAND_ID));
+const checkForOrders = async (client) => {
+  let orders = await client.getOrders(process.env.BANDCAMP_BAND_ID);
+  if (orders?.success === true) {
+    if (orders.items.length === 0) return;
+    orderDetected(orders.items);
+  } else {
+    console.log('Something went wrong');
+  }
 };
 
-let minutes = 0.25;
-
+let minutes = 0.5;
 let interval = minutes * 60 * 1000;
 
 setInterval(() => {
   console.log('Checking...');
-  main(client);
+  checkForOrders(client);
 }, interval);
