eb6a3ec288e00d11195e86b1908f16af9a9cbe96
memarino92 <memarino92@gmail.com>
2022-07-01 17:46:18 -0400

    added timestamps to logs and refactored bandcamp token flow

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ const { PrismaClient } = require('@prisma/client');
 const orderDetected = require('./orderDetected');
 const BandcampClient = require('./bandcamp');
 
-let client = new BandcampClient();
+const bandcampClientId = process.env.BANDCAMP_CLIENT_ID;
+const bandcampClientSecret = process.env.BANDCAMP_CLIENT_SECRET;
+const bandcampBandId = process.env.BANDCAMP_BAND_ID;
+
+const minutes = process.env.INTERVAL_IN_MINUTES || 1;
+const interval = minutes * 60 * 1000;
+
+const client = new BandcampClient(bandcampClientId, bandcampClientSecret);
 
 const db = new PrismaClient();
 
 const checkForOrders = async (client) => {
-  let orders = await client.getOrders(process.env.BANDCAMP_BAND_ID);
+  const orders = await client.getOrders(bandcampBandId);
   if (orders?.success === true) {
+    console.log(
+      `${new Date().toISOString()} Successfully retrieved order list, ${
+        orders.items.length
+      } order(s) found`
+    );
     if (orders.items.length === 0 || orders.items === undefined) return;
     orderDetected(orders.items)
       .catch((e) => {
@@ -19,14 +31,23 @@ const checkForOrders = async (client) => {
         await db.$disconnect();
       });
   } else {
-    console.log('Something went wrong');
+    console.log(
+      `${new Date().toISOString()} Something went wrong retrieving order list`
+    );
   }
 };
 
-let minutes = 0.5;
-let interval = minutes * 60 * 1000;
+const main = async () => {
+  console.log(
+    `${new Date().toISOString()} Checking Bandcamp API for new orders...`
+  );
+  await checkForOrders(client);
+  setInterval(() => {
+    console.log(
+      `${new Date().toISOString()} Checking Bandcamp API for new orders...`
+    );
+    checkForOrders(client);
+  }, interval);
+};
 
-setInterval(() => {
-  console.log('Checking...');
-  checkForOrders(client);
-}, interval);
+main();
